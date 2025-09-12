95c4f277d85e14619a71fa88721055a2d12910dc
vvhao <vovanhao2012vn@gmail.com>
2025-09-13 00:26:41 +0700

    Add git ignore file

--- src/core/mcxa156/frdmmcxa156_dev_hid_generic_freertos/armgcc/debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,9 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "debug")
-  file(REMOVE_RECURSE
-  "dev_hid_generic_freertos.bin"
-  "output.map"
-  )
-endif()
